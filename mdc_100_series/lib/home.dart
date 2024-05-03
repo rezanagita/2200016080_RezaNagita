60d5e0565be40b6f2a3d16f2a6f9e4fa8ea46d9a
REZA NAGITA <rezagitta@com>
2024-05-03 18:51:47 +0700

    create more card

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -4,6 +4,38 @@ class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   // TODO: Make a collection of cards (102)
+  List<Card> _buildGridCards(int count) {
+    List<Card> cards = List.generate(
+      count,
+      (int index) {
+        return Card(
+          clipBehavior: Clip.antiAlias,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 18.0 / 11.0,
+                child: Image.asset('assets/diamond.png'),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const <Widget>[
+                    Text('Title'),
+                    SizedBox(height: 8.0),
+                    Text('Secondary Text'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+    return cards;
+  }
+
   // TODO: Add a variable for Category (104)
   @override
   Widget build(BuildContext context) {
@@ -11,14 +43,44 @@ class HomePage extends StatelessWidget {
     // TODO: Pass Category variable to AsymmetricView (104)
     return Scaffold(
       // TODO: Add app bar (102)
+      appBar: AppBar(
+        title: const Text('Collections'),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            semanticLabel: 'Menu',
+          ),
+          onPressed: () {
+            print('Menu button');
+          },
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.search,
+              semanticLabel: 'search',
+            ),
+            onPressed: () {
+              print('search button');
+            },
+          ),
+          IconButton(
+              icon: const Icon(
+                Icons.tune,
+                semanticLabel: 'filter',
+              ),
+              onPressed: () {
+                print('Filter button');
+              })
+        ],
+      ),
       // TODO: Add a grid view (102)
       body: GridView.count(
-        crossAxisCount: 2, //jumlah kolom
-        padding: const EdgeInsets.all(16.0),
-        childAspectRatio: 8.0 / 9.0,
-        //TODO: Build a grid of cards (102)
-        children: <Widget>[Card()],
-      ),
+          crossAxisCount: 2, //ukuran kolom
+          padding: const EdgeInsets.all(16.0),
+          childAspectRatio: 8.0 / 9.0,
+          //TODO: Build a grid of cards (102)
+          children: _buildGridCards(10)),
       // TODO: Set resizeToAvoidBottomInset (101)
       resizeToAvoidBottomInset: false,
     );
